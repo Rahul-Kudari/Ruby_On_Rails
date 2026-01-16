94d34dea888112fe92f296254f587378120398e8
RAHUL PRAKASH KUDARI <rahulkudari7@gmail.com>
2026-01-16 09:45:29 +0530

    Add files via upload

---
+++ dataTypes.rb
@@ -0,0 +1,104 @@
+#integer , float
+no=100
+no=123.25
+puts no
+puts no.class  # all are objects here
+puts no.object_id  #check the abject address
+
+# mutabel string 
+name="rahul"
+puts name
+
+#Symbol : immutabel 
+id=:hello
+puts id
+
+# nil datatype
+age=nil
+puts age
+
+# arrays 
+arr=[1,2,3,4,5,6];
+arr.each do |a|
+    puts a
+end
+
+# hash types
+hash={:name=>"rahul",:age=>123,:id=>12};
+
+hash.each do |k,v|
+    puts k.to_s+" "+v.to_s
+end
+
+# boolean
+
+flag=true
+puts flag
+
+
+#local variabel
+#global Variabel
+
+$glob="global variabel"  # every where we can use 
+
+class Products 
+
+    @@clsVar=0;
+       
+    @var
+    def initialize(var)
+        @var=var
+        
+
+    end
+    def self.method_Hello()
+        @@clsVar+=1;
+        @@clsVar
+        
+    end
+end
+
+p1=Products.new(1234);
+p2=Products.new(4321);
+
+# puts p1.method_Hello();
+# puts p2.method_Hello();
+
+puts Products.method_Hello()
+puts Products.method_Hello()
+
+PI=3.142
+puts PI
+
+
+# PI=4353  # it through errror
+# puts PI
+
+aim=100
+puts "hhello+""#{aim}";
+
+10.times do
+    puts "sri ram"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
