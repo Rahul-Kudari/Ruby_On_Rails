65558241a89de7a86652251c4195b9c8d0dbb64b
Rahul-Kudari <rahulkudari7@gmail.com>
2026-01-21 15:33:19 +0530

    updated project

--- my_application/app/models/customer.rb
+++ my_application/app/models/customer.rb
@@ -1,7 +1,23 @@
 class Customer < ApplicationRecord
 
-    def check_mail
-        #if the mail is present then fetch else not
+    # Check whether email is present or not
+  def check_email
+    if email.nil? || email.strip.empty?
+      "Email is not present"
+    else
+      "Email is present"
     end
+  end
+
+  # Check whether name contains only alphabetic characters
+  def name_check
+    if name.nil? || name.strip.empty?
+      "Name is empty"
+    elsif name.match?(/\A[a-zA-Z]+\z/)
+      "Name is valid"
+    else
+      "Name must contain only alphabets"
+    end
+  end
     
 end
--- my_application/app/models/product.rb
+++ my_application/app/models/product.rb
@@ -8,6 +8,25 @@ class Product < ApplicationRecord
 
      
   
+    # # write a method inside the modle assign a value to the all collecting value from the product and use select and apply condition if the priceis greater then or select  we 
+  # # 
+  # def self.fetch_active_products
+  # @status = Product.all.limit(10).select { |product| product.stock > 100 }
+  # # @price = Product.all.limit(10).pluck(:price)
+  # # @stock = Product.all.limit(10).pluck(:stock)
+  # end
+  def availablity_check
+    #check the stock value more then zero
+  end
+  def apply_discount(discount_percent)
+    #use case statement and calculate discount .
+  end
+
+  def total_amount 
+    # call the apply_discount and check_availability
+    # if available apply discount. appply any Math methods and apply
+    # return the total value
+  end
     # @price=Product.all.limit(10).pluck(:price)
 
     # @stock=Product.all.limit(20).pluck(:stock);
